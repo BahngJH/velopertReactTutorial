572868f0976ffa530111231ce946b6ea7636c112
Bahng Ji Hoon <wpxm2003@naver.com>
2019-12-03 01:12:16 +0900

    컬러바꾸는거 추가

--- src/Say.js
+++ src/Say.js
@@ -4,11 +4,23 @@ const Say = () => {
   const [message, setMessage] = useState("");
   const onClickEnter = () => setMessage("안녕하세요");
   const onClickleave = () => setMessage("안녕히가세요");
+
+  const [color, setColor] = useState("black");
+
   return (
     <div>
       <button onClick={onClickEnter}>입장</button>
       <button onClick={onClickleave}>퇴장</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        red
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        blue
+      </button>
+      <button style={{ color: "green" }} onClick={() => setColor("green")}>
+        green
+      </button>
     </div>
   );
 };
