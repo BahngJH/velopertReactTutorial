d16c16c814c55f2ef4ccc8faddbfc8adc0c7c7af
Bahng Ji Hoon <wpxm2003@naver.com>
2019-12-04 00:17:38 +0900

    Event part#2 function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Counter from "./Counter";
-import Say from "./Say";
+//import Counter from "./Counter";
+//import Say from "./Say";
+//import EventPractice_class from "./EventPractice_class";
+import EventPractice_function from "./EventPractice_function";
 
 const App = () => {
-  return <Say />;
+  return <EventPractice_function />;
 };
 
 export default App;
--- src/EventPractice_class.js
+++ src/EventPractice_class.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
 
 class EventPractice_class extends Component {
+  state = {
+    username: "",
+    message: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  hendleClick = () => {
+    alert(this.state.username + " : " + this.state.message);
+    this.setState({
+      message: "",
+      username: ""
+    });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.hendleClick}>확인하기</button>
+      </div>
+    );
   }
 }
 
